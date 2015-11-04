01aa5543f951b43433c2d662cfda61b4a6acec68
Ignazio Setti <ignazio.setti@gmail.com>
2015-11-04 21:47:31 +0100

    Configure phpunit and add a staminal DiffHelper class

---
+++ tests/diffTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Karadiff\Helpers\DiffHelper;
+
+class DiffTest extends PHPUnit_Framework_TestCase
+{
+    public function testWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
