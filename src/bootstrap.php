cc132fde463349b85e7b3b74f091a4408b0a721f
Ignazio Setti <ignazio.setti@gmail.com>
2015-11-04 17:48:43 +0100

    Add a global error handler

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,31 @@
 <?php
 
+namespace Karadiff;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Env can be one in development|test|production
+ */
+$env = isset($_ENV['env']) ? $_ENV['env']: 'development';
+
+if ($env === 'development') {
+    error_reporting(E_ALL);
+}
+
+/**
+ * Register a global error handler
+ */
+$whoops = new \Whoops\Run;
+
+if ($env !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e) {
+        echo 'Friendly error page and send an email to the developer';
+    });
+}
+
+$whoops->register();
+
+throw new \Exception;
