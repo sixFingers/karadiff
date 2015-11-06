8a2f492fcdc0a8c81e8fdb05e8338b3a6a55fa9d
Ignazio Setti <ignazio.setti@gmail.com>
2015-11-06 14:34:44 +0100

    Add another renderer which prints out a side by side view, and hook it up to our controller

--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -4,33 +4,31 @@ namespace Karadiff\Controllers;
 
 use Karadiff\Helpers\TemplateHelper;
 use Karadiff\Diff\Providers\DiffProviderStringWord;
-use Karadiff\Diff\Renderers\DiffRendererTextWord;
+use Karadiff\Diff\Renderers\DiffRendererSideBySide;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $original = $this->request->request->get('original', '');
-        $changed = $this->request->request->get('changed', '');
-        $additions = 0;
-        $removals = 0;
+        $sides = ['', ''];
+        $additionsCount = $removalsCount = 0;
 
         if($this->request->isMethod(Request::METHOD_POST)) {
+            $original = $this->request->request->get('original', '');
+            $changed = $this->request->request->get('changed', '');
             $provider = new DiffProviderStringWord($original, $changed);
-            $renderer = new DiffRendererTextWord($provider);
-            $diff = $renderer->render();
-            $additions = $provider->additions;
-            $removals = $provider->removals;
+            $renderer = new DiffRendererSideBySide($provider);
+            $sides = $renderer->render();
+            $additionsCount = $provider->additions;
+            $removalsCount = $provider->removals;
         }
 
         $content = TemplateHelper::render('welcome.html', array(
             'title' => 'Diff',
-            'original' => $original,
-            'changed' => $changed,
-            'diff' => $diff,
-            'additions' => $additions,
-            'removals' => $removals,
+            'sides' => $sides,
+            'additionsCount' => $additionsCount,
+            'removalsCount' => $removalsCount,
         ));
 
         $this->response->setContent($content);
